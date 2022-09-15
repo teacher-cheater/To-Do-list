46f294b9dd14c34df0724601c0a7b0a5bbec9efc
teacher_cheater <johnnykayfun@gmail.com>
2022-09-15 19:04:41 +0300

    clean code

--- script.js/draw-task.js
+++ script.js/draw-task.js
@@ -5,37 +5,32 @@ import { items, btnDelTaskComplete } from './variables.js'
 //TODO/*-----------------функция для отрисовки задач---------*/
 export function showTask(idTask, nameTask, isDoneTask) {
    //блок для checkbox'ов
-   let checkBoxContent = document.createElement('div')
+   const checkBoxContent = document.createElement('div')
    checkBoxContent.classList = 'check__box-content'
 
-   let textP = document.createElement('label') //label для содержимого
+   const textP = document.createElement('label') //label для содержимого
 
    textP.setAttribute('for', "check__choice_" + idTask)//добавление ID к задаче
    textP.classList = "check__choice"
    items.append(textP) //добавлен в блок 'main__item-del'
 
-   let checkbox = document.createElement('input') // checkbox для выбора задач
+   const checkbox = document.createElement('input') // checkbox для выбора задач
    checkbox.setAttribute('type', 'checkbox')
-   checkbox.id = "check__choice_" + idTask
+   checkbox.id = `check__choice_${idTask}`
    checkbox.classList = "check__input"
    textP.append(checkbox) //добавлен в label
 
    textP.innerText = nameTask
    checkBoxContent.append(checkbox)
 
-   let div = document.createElement('div') //блок для картинки 'X'
+   const div = document.createElement('div') //блок для картинки 'X'
    div.classList = 'main__item-del'
    div.dataset.id = idTask // создали аттрибут(понимать, какую запись мы удаляем)
    div.textContent = '❌'
    items.append(checkBoxContent) //добавлен в блок 'main__item-del'
    checkBoxContent.append(textP)
    checkBoxContent.append(div)
-   if (isDoneTask === 1) {//проверка на checked
-      checkbox.checked = true
-   }
-   else {
-      checkbox.checked = false
-   }
+   checkbox.checked = isDoneTask === 1;
    //TODO/*------- удалениe завершенных задач---------*/
    btnDelTaskComplete.addEventListener('click', sortOut)
 
--- script.js/functions.js
+++ script.js/functions.js
@@ -4,8 +4,8 @@ import { postDelId } from './fetch-files.js';
 
 //TODO/*--------------функция для удаления task'ов-------------------------------*/
 export function delData(targets) {
-   let id = targets.attributes[1].nodeValue //получили ID-шник, который хранится в записи.
-   fetch('http://24api.ru/rest-todo/' + id, {
+   const id = targets.attributes[1].nodeValue //получили ID-шник, который хранится в записи.
+   fetch(`http://24api.ru/rest-todo/${id}`, {
       method: 'DELETE',
    }).then(() => {
       targets.parentElement.remove() // удаляем div 
@@ -14,12 +14,12 @@ export function delData(targets) {
 
 //TODO/*------------функция для обращения к ID элементам---------------------*/
 export function sortOut() {
-   let elements = document.querySelectorAll('.check__box-content')
-   let array = []
+   const elements = document.querySelectorAll('.check__box-content')
+   const array = []
    for (let k of elements) {
       if (k.childNodes[0].checked == true) {
-         let sort = k.childNodes[2]
-         let sortSplit = sort.dataset.id
+         const sort = k.childNodes[2]
+         const sortSplit = sort.dataset.id
          array.push(sortSplit)
       }
    }
